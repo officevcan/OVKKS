881ab13d75574be90e34fb83645550d99d6f5607
admin <officevcan@gmail.com>
2016-09-23 10:42:20 +0530

    OVKKSApp

--- app/src/main/java/in/co/officevcan/stg1/ovkksapp/SplashActivity.java
+++ app/src/main/java/in/co/officevcan/stg1/ovkksapp/SplashActivity.java
@@ -1,13 +1,15 @@
 package in.co.officevcan.stg1.ovkksapp;
 
+import android.app.Activity;
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
