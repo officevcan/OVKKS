7b563be82549ec23b9bc24955330d8ed308705ff
admin <officevcan@gmail.com>
2016-09-23 14:13:22 +0530

    OVKKSApp

--- app/src/main/java/in/co/officevcan/stg1/ovkksapp/AboutActivity.java
+++ app/src/main/java/in/co/officevcan/stg1/ovkksapp/AboutActivity.java
@@ -1,8 +1,11 @@
 package in.co.officevcan.stg1.ovkksapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 /**
@@ -10,6 +13,7 @@ import android.webkit.WebView;
  */
 public class AboutActivity extends AppCompatActivity
 {
+    Toolbar app_bar;
     Context context;
     WebView webview1;
     protected void onCreate(Bundle savedInstanceSate)
@@ -25,5 +29,21 @@ public class AboutActivity extends AppCompatActivity
         webview1.loadUrl("file:///android_asset/about.html");
 
         context = getApplicationContext();
+
+        app_bar = (Toolbar) findViewById(R.id.app_bar);
+        app_bar.setTitle("About Us");
+
+        setSupportActionBar(app_bar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent ii = new Intent(getApplicationContext(), WelcomeActivity.class);
+        startActivity(ii);
+        return super.onOptionsItemSelected(item);
     }
 }
