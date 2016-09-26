8876b7f91a228288e7d74ed0ed1990c9eeee10bf
admin <officevcan@gmail.com>
2016-09-26 13:04:05 +0530

    OVKKSApp

--- app/src/main/java/in/co/officevcan/stg1/ovkksapp/AboutActivity.java
+++ app/src/main/java/in/co/officevcan/stg1/ovkksapp/AboutActivity.java
@@ -3,9 +3,12 @@ package in.co.officevcan.stg1.ovkksapp;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.Window;
 import android.webkit.WebView;
 
 /**
@@ -15,18 +18,26 @@ public class AboutActivity extends AppCompatActivity
 {
     Toolbar app_bar;
     Context context;
-    WebView webview1;
+   // WebView webview1;
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
     protected void onCreate(Bundle savedInstanceSate)
     {
         super.onCreate(savedInstanceSate);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_about);
 
 
-        webview1=(WebView)findViewById(R.id.webview1);
-        webview1.getSettings().setJavaScriptEnabled(true);
+      //  webview1=(WebView)findViewById(R.id.webview1);
+
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.containerView, new AboutUsTabLayoutFragment()).commit();
+
+      //  webview1.getSettings().setJavaScriptEnabled(true);
 
         // uri loding in webview
-        webview1.loadUrl("file:///android_asset/about.html");
+       // webview1.loadUrl("file:///android_asset/about.html");
 
         context = getApplicationContext();
 
---
+++ app/src/main/java/in/co/officevcan/stg1/ovkksapp/BirthOfKksFragment.java
@@ -0,0 +1,33 @@
+package in.co.officevcan.stg1.ovkksapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BirthOfKksFragment extends Fragment {
+
+
+
+    public BirthOfKksFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view= inflater.inflate(R.layout.fragment_birth_of_kks, container, false);
+
+        return view;
+    }
+
+}
